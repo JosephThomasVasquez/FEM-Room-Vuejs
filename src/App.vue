d7a590413d598b743da280b68d0c12fc55a92e92
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2020-11-09 20:45:54 -0800

    Adjusted styling and content for responsive design

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div id="app">
     <div id="nav">
-      <div class="title-name">Room</div>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/shop">Shop</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/contact">Contact</router-link>
+      <div class="nav-bar">
+        <div class="title-name">room</div>
+        <router-link to="/">Home</router-link>
+        <router-link to="/shop">Shop</router-link>
+        <router-link to="/about">About</router-link>
+        <router-link to="/contact">Contact</router-link>
+      </div>
     </div>
     <router-view />
   </div>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,7 +9,16 @@
       <HelloWorld msg="Room" />
     </div>
     <div class="section-headline">
-      <h1>Discover innovative ways to decorate</h1>
+      <div class="container-headline">
+        <h1 class="headline-h1">Discover innovative ways to decorate</h1>
+        <p class="headline-subtext">
+          We provide unmatched quality, comfort, and style for property owners
+          across the country. Our experts combine form andnfunction in bringing
+          your vision to life. Create a room in your own style with our
+          collection and make your property a reflection of you and what you
+          love.
+        </p>
+      </div>
     </div>
   </div>
 </template>
